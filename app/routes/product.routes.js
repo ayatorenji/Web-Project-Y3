00b5bd406e87fdabf4d7a6daecf5b5b224355811
Jay <koonjjkung.castgame@gmail.com>
2023-10-07 03:34:16 +0700

    first commit

---
+++ app/routes/product.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.get('/products', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
+
+// Add more routes as needed
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+global.__basedir = __dirname;
+var corsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res)=>{
+    res.json({ message: "Welcome to my game currency shop API."});
+});
+require("./app/routes/user.routes")(app);
+require("./app/routes/file.routes")(app);
+require("./app/routes/productRoutes")(app);  // Add this line
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("Server is running on port " + PORT);
+});
